907c9a11da77cd07347398ee4ca159300e48706e
Joao Victor <jvictoreliasc@gmail.com>
2022-02-12 19:00:59 -0300

    Adicionando opcao de GET_ONE

--- SocketClient/SocketClient.py
+++ SocketClient/SocketClient.py
@@ -4,17 +4,23 @@ import socket
 
 HOST = '127.0.0.1'  # The server's hostname or IP address
 PORT = 65432        # The port used by the server
-TYPE = "GET"
+
+TYPE = "GET_ONE"
 FILE_NAME = "OnePiece.jpg"
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
     sock.connect((HOST, PORT))
 
-    if (TYPE == "GET"):
-        sock.send(FILE_NAME.encode())
-        with open(FILE_NAME, 'wb') as file:
-            while (True):
-                data = sock.recv(16777216)
-                if not data:
-                    break
-                file.write(data)
+    if (TYPE == "GET_ONE"):
+        sock.send(TYPE.encode())
+        response = sock.recv(1024)
+        if (response.decode() == 'OK'):
+            # pedido do arquivo
+            sock.send(FILE_NAME.encode())
+            with open(FILE_NAME, 'wb') as file:
+                while (True):
+                    data = sock.recv(16777216)
+                    if not data:
+                        break
+                    file.write(data)
+                    
--- SocketServer/SocketServer.py
+++ SocketServer/SocketServer.py
@@ -8,12 +8,18 @@ PORT = 65432
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
 	sock.bind((HOST, PORT))
+	# while (True): - GETONE GETLIST PUT
 	sock.listen()
 	conn, addr = sock.accept()
-	with conn:
-		print('Connected by', addr)
-		FILE_NAME = conn.recv(1024).decode()
+	TYPE = conn.recv(1024).decode()
 
-		with open(FILE_NAME, 'rb') as file:
-			for data in file.readlines():
-				conn.send(data)
+	if (TYPE == "GET_ONE"):
+		conn.send("OK".encode())
+
+		with conn:
+			print('Connected by', addr)
+			FILE_NAME = conn.recv(1024).decode()
+
+			with open(FILE_NAME, 'rb') as file:
+				for data in file.readlines():
+					conn.send(data)
