9b009a52193ad9c85abe0541d909a17d826a1604
Joao Victor <jvictoreliasc@gmail.com>
2022-02-12 18:08:46 -0300

    Socket Client inicial

---
+++ SocketClient/SocketClient.py
@@ -0,0 +1,20 @@
+#Joao Victor Elias Costa
+#760927
+import socket
+
+HOST = '127.0.0.1'  # The server's hostname or IP address
+PORT = 65432        # The port used by the server
+TYPE = "GET"
+FILE_NAME = "OnePiece.jpg"
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+    sock.connect((HOST, PORT))
+
+    if (TYPE == "GET"):
+        sock.send(FILE_NAME.encode())
+        with open(FILE_NAME, 'wb') as file:
+            while (True):
+                data = sock.recv(16777216)
+                if not data:
+                    break
+                file.write(data)
