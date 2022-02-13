f501c58b1798c4ff9ad8dacd55d251b0ca06fa73
Joao Victor <jvictoreliasc@gmail.com>
2022-02-13 04:04:42 -0300

    Menu simples

--- SocketClient/SocketClient.py
+++ SocketClient/SocketClient.py
@@ -4,44 +4,65 @@ import socket
 
 HOST = '127.0.0.1'  # The server's hostname or IP address
 PORT = 65432        # The port used by the server
+TYPE = ""
 
-TYPE = "PUT_ONE"
-FILE_NAME = "OnePiece.jpg"
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-    sock.connect((HOST, PORT))
+while (TYPE != "SAIR"):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock.connect((HOST, PORT))
+        print(f"Escolha uma das seguintes opcoes:\n1 - Listar arquivos do servidor.\n2 - Baixar um arquivo do servidor.\n3 - Enviar um arquivo ao servidor.\n4 - Sair")
+        opcao = input()
 
-    if (TYPE == "GET_ONE"):
-        sock.send(TYPE.encode())
-        response = sock.recv(1024)
-        if (response.decode() == 'OK'):
-            # pedido do arquivo
-            sock.send(FILE_NAME.encode())
-            with open(FILE_NAME, 'wb') as file:
-                while (True):
-                    data = sock.recv(16777216)
-                    if not data:
-                        break
-                    file.write(data)
+        # POSSIVEIS OPCOES
+        if (opcao == "1"):
+            TYPE = "GET_LIST"
+        elif (opcao == "2"):
+            TYPE = "GET_ONE"
+            FILE_NAME = input("Digite o nome do arquivo a ser BAIXADO: ")
+        elif (opcao == "3"):
+            TYPE = "PUT_ONE"
+            FILE_NAME = input("Digite o nome do arquivo a ser ENVIADO: ")
+        elif (opcao == "4"):
+            TYPE = "SAIR"
+        else:
+            print("Opcao invalida.")
 
-    if (TYPE == "GET_LIST"):
-        sock.send(TYPE.encode())
-        dir_string = sock.recv(16777216).decode()
-        dir_list = dir_string.split(",")
-        dir_list.remove("SocketServer.py")
-        print("Arquivos no servidor:")
-        for dir in dir_list:
-            print(dir)
+        if (TYPE == "GET_LIST"):
+            sock.send(TYPE.encode())
+            dir_string = sock.recv(16777216).decode()
+            dir_list = dir_string.split(",")
+            dir_list.remove("SocketServer.py")
+            print(f"\nArquivos no servidor:")
+            for dir in dir_list:
+                print("- " + dir)
+            print(f"_________________________________\n")
 
-    if (TYPE == "PUT_ONE"):
-        sock.send(TYPE.encode())
-        response = sock.recv(1024)
-        if (response.decode() == 'OK'):
-            
-            # envia o nome do arquivo
-            sock.send(FILE_NAME.encode())
-            
-            # envia o arquivo
-            with open(FILE_NAME, 'rb') as file:
-                for data in file.readlines():
-                    sock.send(data)
+        if (TYPE == "GET_ONE"):
+            sock.send(TYPE.encode())
+            response = sock.recv(1024)
+            if (response.decode() == 'OK'):
+                # pedido do arquivo
+                sock.send(FILE_NAME.encode())
+                with open(FILE_NAME, 'wb') as file:
+                    while (True):
+                        data = sock.recv(16777216)
+                        if not data:
+                            break
+                        file.write(data)
+                    print("Arquivo baixado com sucesso.\n")
+                    print(f"_________________________________\n")
+
+        if (TYPE == "PUT_ONE"):
+            sock.send(TYPE.encode())
+            response = sock.recv(1024)
+            if (response.decode() == 'OK'):
+                
+                # envia o nome do arquivo
+                sock.send(FILE_NAME.encode())
+
+                # envia o arquivo
+                with open(FILE_NAME, 'rb') as file:
+                    for data in file.readlines():
+                        sock.send(data)
+                    print("Arquivo enviado com sucesso.\n")
+                    print(f"_________________________________\n")
--- SocketServer/SocketServer.py
+++ SocketServer/SocketServer.py
@@ -9,47 +9,47 @@ PORT = 65432
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
 	sock.bind((HOST, PORT))
-	# while (True): - GETONE GETLIST PUT
-	sock.listen()
-	conn, addr = sock.accept()
-	TYPE = conn.recv(1024).decode()
+	while (True):
+		sock.listen()
+		conn, addr = sock.accept()
+		TYPE = conn.recv(1024).decode()
 
-	if (TYPE == "GET_ONE"):
-		conn.send("OK".encode())
+		if (TYPE == "GET_ONE"):
+			conn.send("OK".encode())
 
-		with conn:
-			print('Connected by', addr)
-			FILE_NAME = conn.recv(1024).decode()
+			with conn:
+				print('Connected by', addr)
+				FILE_NAME = conn.recv(1024).decode()
 
-			with open(FILE_NAME, 'rb') as file:
-				for data in file.readlines():
-					conn.send(data)
+				with open(FILE_NAME, 'rb') as file:
+					for data in file.readlines():
+						conn.send(data)
 
-	if (TYPE == "GET_LIST"):
-		with conn:
-			print('Connected by', addr)
-			local_dir = os.path.dirname(os.path.realpath(__file__))
-			dir_string = ""
+		if (TYPE == "GET_LIST"):
+			with conn:
+				print('Connected by', addr)
+				local_dir = os.path.dirname(os.path.realpath(__file__))
+				dir_string = ""
 
-			# adiciona os itens do diretorio na lista
-			for directory, subfolders, files in os.walk(local_dir):
-				for file in files:
-					if (dir_string == ""):
-						dir_string = file
-					else:
-						dir_string = dir_string + "," + file			
-			conn.send(dir_string.encode())
+				# adiciona os itens do diretorio na lista
+				for directory, subfolders, files in os.walk(local_dir):
+					for file in files:
+						if (dir_string == ""):
+							dir_string = file
+						else:
+							dir_string = dir_string + "," + file			
+				conn.send(dir_string.encode())
 
-	if (TYPE == "PUT_ONE"):
-		conn.send("OK".encode())
+		if (TYPE == "PUT_ONE"):
+			conn.send("OK".encode())
 
-		with conn:
-			print('Connected by', addr)
-			FILE_NAME = conn.recv(1024).decode()
-			if FILE_NAME:
-				with open(FILE_NAME, 'wb') as file:
-					while (True):
-						data = conn.recv(16777216)
-						if not data:
-							break
-						file.write(data)
+			with conn:
+				print('Connected by', addr)
+				FILE_NAME = conn.recv(1024).decode()
+				if FILE_NAME:
+					with open(FILE_NAME, 'wb') as file:
+						while (True):
+							data = conn.recv(16777216)
+							if not data:
+								break
+							file.write(data)
